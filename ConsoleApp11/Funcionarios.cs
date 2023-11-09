3d7dcfbd1782abb44cc370c6cc6b7b3bbe5f762f
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-08 22:05:33 -0300

    Add project files.

---
+++ ConsoleApp11/Funcionarios.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ConsoleApp11 {
+    internal class Funcionarios {
+        public string nome1;
+        public string nome2;
+        public double salario1;
+        public double salario2;
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp11;
+using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Funcionarios x;
+
+            x = new Funcionarios();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            x.nome1 = Console.ReadLine();
+            x.salario1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            x.nome2 = Console.ReadLine();
+            x.salario2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double mediaDoSalario = (x.salario1 + x.salario2) / 2;
+            Console.WriteLine("Salário médio = " + mediaDoSalario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
